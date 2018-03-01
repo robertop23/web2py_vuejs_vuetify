ea09ba7b9f74afb4ee89c6b96ffcbe44beff89c8
Roberto Perdomo <robertop23@gmail.com>
2018-03-01 11:20:05 -0400

    removed main from 404

--- resources/assets/js/pages/errors/404.vue
+++ resources/assets/js/pages/errors/404.vue
@@ -1,18 +1,16 @@
 <template>
-  <main>
-    <v-content>
-      <v-container fluid>
-        <v-layout column align-center>
-          <div class="display-3 grey--text mt-5">{{ $t('page_not_found') }}</div>
-          <div class="body-2 my-3">
-            <router-link :to="{ name: 'welcome' }">
-              {{ $t('go_home') }}
-            </router-link>
-          </div>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </main>
+  <v-content>
+    <v-container fluid>
+      <v-layout column align-center>
+        <div class="display-3 grey--text mt-5">{{ $t('page_not_found') }}</div>
+        <div class="body-2 my-3">
+          <router-link :to="{ name: 'welcome' }">
+            {{ $t('go_home') }}
+          </router-link>
+        </div>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
