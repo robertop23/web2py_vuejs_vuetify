287fbb25e07a924823e2d9f5e5f616282cb225dd
Roberto Perdomo <robertop23@gmail.com>
2018-03-01 16:55:55 -0400

    added change_password

--- controllers/api.py
+++ controllers/api.py
@@ -1,6 +1,7 @@
 import json
 from gluon.tools import AuthJWT, AuthAPI
 import requests
+import hashlib
 
 myjwt = AuthJWT(auth, secret_key='secret')
 
@@ -69,16 +70,15 @@ def logout():
 
 @api_requires_login
 def user():
-    import hashlib
     avatar = 'https://www.gravatar.com/avatar/{}.jpg?s=200&d=mm'.format(
         hashlib.md5(auth.user.email.lower()).hexdigest())
-    user_data = {'first_name': auth.user.first_name, 'email': auth.user.email, 'avatar': avatar}
+    user_data = {'first_name': auth.user.first_name,
+                 'last_name': auth.user.last_name, 'email': auth.user.email, 'avatar': avatar}
     return user_data
 
 
 @api_requires_extension
 def register():
-
     first_name = request.vars.first_name
     last_name = request.vars.last_name
     email = request.vars.email
@@ -94,6 +94,35 @@ def register():
         error = ['{}'.format(k, v) for k, v in authentication['errors'].iteritems()][0]
         raise HTTP(400, error.replace('_', ' ').lower().capitalize())
 
+
+@api_requires_login
+def profile():
+    first_name = request.vars.first_name
+    last_name = request.vars.last_name
+    authentication = AuthAPI(db).profile(first_name=first_name, last_name=last_name)
+    if authentication['message'] == 'Profile updated':
+        avatar = 'https://www.gravatar.com/avatar/{}.jpg?s=200&d=mm'.format(
+            hashlib.md5(auth.user.email).hexdigest())
+        authentication['user']['avatar'] = avatar
+        return authentication
+    else:
+        error = ['{}'.format(v) for k, v in authentication['errors'].iteritems()][0]
+        raise HTTP(400, error.replace('_', ' ').lower().capitalize())
+
+
+@api_requires_login
+def change_password():
+    old_password = request.vars.old_password
+    new_password = request.vars.new_password
+    new_password2 = request.vars.new_password2
+    authentication = AuthAPI(db).change_password(old_password=old_password,
+                                                 new_password=new_password,
+                                                 new_password2=new_password)
+    if authentication['message'] == 'Password changed':
+        return authentication
+    else:
+        error = ['{}'.format(v) for k, v in authentication['errors'].iteritems()][0]
+        raise HTTP(400, error.replace('_', ' ').lower().capitalize())
 # Test jwt
 
 
